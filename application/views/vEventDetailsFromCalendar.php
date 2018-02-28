e96aedca487fd54e9fa9aa24dde155780ff61db7
jenadrianne <yan4414@gmail.com>
2018-02-27 22:05:36 -0800

    fixes to display calendar

--- application/views/vEventDetailsFromCalendar.php
+++ application/views/vEventDetailsFromCalendar.php
@@ -335,16 +335,6 @@ foreach($going as $g){
                                     </ul>
                                 </div>
                             </div>
-
-                            <div class="panel panel-default sidebar-menu wow fadeInRight animated">
-                                <div class="panel-heading">
-                                    <h3 class="panel-title">Ads here</h3>
-                                </div>
-                                <div class="panel-body recent-property-widget">
-                                    <img src="assets/img/ads.jpg">
-                                </div>
-                            </div>
-
                             <!-- <div class="panel panel-default sidebar-menu wow fadeInRight animated" >
                                 <div class="panel-heading">
                                 <h3 class="panel-title">Search</h3>
