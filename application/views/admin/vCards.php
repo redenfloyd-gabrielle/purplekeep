0a4e08a5a95360114b3ae6032d945d45922c7eb4
algreg1998 <algreg17@gmail.com>
2018-03-01 14:31:05 +0800

    placed the table in the panel

--- application/views/admin/vCards.php
+++ application/views/admin/vCards.php
@@ -9,46 +9,54 @@
     <div id="content">
         <div class="row">
           <div class="col-lg-12">
-          <table class="table table-bordered table-hover text-center" id = "card-table" data-card-url="<?php echo site_url();?>/admin/CAdmin/fetchCardData">
-									<thead>
-										<tr>
-											<th>ID</th>
-                      <th>Code</th>
-                      <th>Amount</th>
-                      <th>Status</th>
-                      <th>Created By</th>
-                      <th>Updated By</th>
-                      <th>Added On</th>
-                      <th>Updated On</th>
-										</tr>
-									</thead>
+                <section class="panel">
+                    <header class="panel-heading">
+                        <h2><strong>Cards</strong></h2>
+                    </header>
+                    <div class="panel-body">
+                      <table class="table table-bordered table-hover text-center" id = "card-table" data-card-url="<?php echo site_url();?>/admin/CAdmin/fetchCardData">
+                              <thead>
+                                <tr>
+                                  <th>ID</th>
+                                  <th>Code</th>
+                                  <th>Amount</th>
+                                  <th>Status</th>
+                                  <th>Created By</th>
+                                  <th>Updated By</th>
+                                  <th>Added On</th>
+                                  <th>Updated On</th>
+                                </tr>
+                              </thead>
 
-									<tbody >
-                  <?php
-                    if($dtCards != False){
-                      $prepareDat = "";
-                    
-                      foreach ($dtCards as $cards) {
-                        
-                        $statCards = ($cards->cardStatus == 0)?'<span class="label label-danger">Used</span>':'<span class="label label-success">Available</span>';
-                        $nameCard = $cards->first_name. " ". $cards->last_name;
-                        $prepareDat .= "<tr>".
-                                          "<td>".$cards->cardId."</td>".
-                                          "<td>".$cards->cardCode."</td>".
-                                          "<td>".$cards->cardAmount."</td>".
-                                          "<td>".$statCards."</td>".
-                                          "<td>".$nameCard."</td>".
-                                          "<td>".$nameCard."</td>".
-                                          "<td>".$cards->addedAt."</td>".
-                                          "<td>".$cards->updatedAt."</td>".
-                                        "</tr>";
-                      }
-                       echo $prepareDat;   
-                    }
-                  ?>
-									</tbody>
-								</table>
-          </div>
+                              <tbody >
+                              <?php
+                                if($dtCards != False){
+                                  $prepareDat = "";
+                                
+                                  foreach ($dtCards as $cards) {
+                                    
+                                    $statCards = ($cards->cardStatus == 0)?'<span class="label label-danger">Used</span>':'<span class="label label-success">Available</span>';
+                                    $nameCard = $cards->first_name. " ". $cards->last_name;
+                                    $prepareDat .= "<tr>".
+                                                      "<td>".$cards->cardId."</td>".
+                                                      "<td>".$cards->cardCode."</td>".
+                                                      "<td>".$cards->cardAmount."</td>".
+                                                      "<td>".$statCards."</td>".
+                                                      "<td>".$nameCard."</td>".
+                                                      "<td>".$nameCard."</td>".
+                                                      "<td>".$cards->addedAt."</td>".
+                                                      "<td>".$cards->updatedAt."</td>".
+                                                    "</tr>";
+                                  }
+                                   echo $prepareDat;   
+                                }
+                              ?>
+                              </tbody>
+                      </table>
+                    </div>
+                </section>
+            </div>
+
         </div>
         <div class="row">
             <div class="col-lg-12">
@@ -58,73 +66,73 @@
                     </header>
                     <div class="panel-body">
 
-                    <form id="gForm" class="form inline" method="POST" action="<?php echo site_url('finance/CCard/card'); ?>">
-                        <table cellpadding="0" cellspacing="0" border="0" class="table table-striped table-hover" id="toggle-column table-example">
-                            <thead>
-                                <tr>
-                                  <th>Amount</th>
-                                  <th>Quantity</th>
-                                </tr>
-                            </thead>
-                            
-                            <tbody>
-                                <tr>
-                                    <td>&#x20B1; 100</td>
-                                    <td>
-                                        <span class="input-group-btn form-group">
-                                           <button class="btn btn-secondary col-1 cardsbtn unaM" type="button"><i class="fa fa-minus" aria-hidden="true"></i></button> 
-                                           <input type="text" name="qty1" class="form-control col-sm-3 qtyinput" id="qty1" placeholder="Quantity" aria-label="Quantity">
-                                           <button class="btn btn-secondary col-1 cardsbtn unaP" type="button"><i class="fa fa-plus" aria-hidden="true"></i></button>
-                                        </span>
-                                    </td>
-                                </tr>
+                      <form id="gForm" class="form inline" method="POST" action="<?php echo site_url('finance/CCard/card'); ?>">
+                          <table cellpadding="0" cellspacing="0" border="0" class="table table-striped table-hover" id="toggle-column table-example">
+                              <thead>
+                                  <tr>
+                                    <th>Amount</th>
+                                    <th>Quantity</th>
+                                  </tr>
+                              </thead>
+                              
+                              <tbody>
+                                  <tr>
+                                      <td>&#x20B1; 100</td>
+                                      <td>
+                                          <span class="input-group-btn form-group">
+                                             <button class="btn btn-secondary col-1 cardsbtn unaM" type="button"><i class="fa fa-minus" aria-hidden="true"></i></button> 
+                                             <input type="text" name="qty1" class="form-control col-sm-3 qtyinput" id="qty1" placeholder="Quantity" aria-label="Quantity">
+                                             <button class="btn btn-secondary col-1 cardsbtn unaP" type="button"><i class="fa fa-plus" aria-hidden="true"></i></button>
+                                          </span>
+                                      </td>
+                                  </tr>
 
-                                <tr>
-                                    <td>&#x20B1; 200</td> 
-                                    <td>
-                                        <span class="input-group-btn form-group">
-                                           <button class="btn btn-secondary col-1 dosM" type="button"><i class="fa fa-minus" aria-hidden="true"></i></button>
-                                           <input type="text" name="qty2" class="form-control col-sm-3 qtyinput" id="qty2" placeholder="Quantity" aria-label="Quantity">
-                                           <button class="btn btn-secondary col-1 dosP" type="button"><i class="fa fa-plus" aria-hidden="true"></i></button>
-                                        </span>
-                                    </td>
-                                </tr>
+                                  <tr>
+                                      <td>&#x20B1; 200</td> 
+                                      <td>
+                                          <span class="input-group-btn form-group">
+                                             <button class="btn btn-secondary col-1 dosM" type="button"><i class="fa fa-minus" aria-hidden="true"></i></button>
+                                             <input type="text" name="qty2" class="form-control col-sm-3 qtyinput" id="qty2" placeholder="Quantity" aria-label="Quantity">
+                                             <button class="btn btn-secondary col-1 dosP" type="button"><i class="fa fa-plus" aria-hidden="true"></i></button>
+                                          </span>
+                                      </td>
+                                  </tr>
 
-                                <tr>
-                                    <td>&#x20B1; 500</td>
-                                    <td>
-                                        <span class="input-group-btn form-group">
-                                           <button class="btn btn-secondary col-1 tresM" type="button"><i class="fa fa-minus" aria-hidden="true"></i></button>
-                                           <input type="text" name="qty3" class="form-control col-sm-3 qtyinput" id="qty3" placeholder="Quantity" aria-label="Quantity">
-                                           <button class="btn btn-secondary col-1 tresP" type="button"><i class="fa fa-plus" aria-hidden="true"></i></button>
-                                        </span>
-                                    </td>
-                                </tr>
+                                  <tr>
+                                      <td>&#x20B1; 500</td>
+                                      <td>
+                                          <span class="input-group-btn form-group">
+                                             <button class="btn btn-secondary col-1 tresM" type="button"><i class="fa fa-minus" aria-hidden="true"></i></button>
+                                             <input type="text" name="qty3" class="form-control col-sm-3 qtyinput" id="qty3" placeholder="Quantity" aria-label="Quantity">
+                                             <button class="btn btn-secondary col-1 tresP" type="button"><i class="fa fa-plus" aria-hidden="true"></i></button>
+                                          </span>
+                                      </td>
+                                  </tr>
 
-                                <tr>
-                                    <td>&#x20B1; 1000</td>
-                                    <td>
-                                        <span class="input-group-btn form-group">
-                                           <button class="btn btn-secondary col-1 kwatroM" type="button"><i class="fa fa-minus" aria-hidden="true"></i></button>
-                                           <input type="text" name="qty4" class="form-control col-sm-3 qtyinput" id="qty4" placeholder="Quantity" aria-label="Quantity">
-                                           <button class="btn btn-secondary col-1 kwatroP" type="button"><i class="fa fa-plus" aria-hidden="true"></i></button>
-                                        </span>
-                                    </td>
-                                </tr>   
-                            </tbody>
-                        </table>
+                                  <tr>
+                                      <td>&#x20B1; 1000</td>
+                                      <td>
+                                          <span class="input-group-btn form-group">
+                                             <button class="btn btn-secondary col-1 kwatroM" type="button"><i class="fa fa-minus" aria-hidden="true"></i></button>
+                                             <input type="text" name="qty4" class="form-control col-sm-3 qtyinput" id="qty4" placeholder="Quantity" aria-label="Quantity">
+                                             <button class="btn btn-secondary col-1 kwatroP" type="button"><i class="fa fa-plus" aria-hidden="true"></i></button>
+                                          </span>
+                                      </td>
+                                  </tr>   
+                              </tbody>
+                          </table>
 
-                        <center>
-                            <button id="generate" class="btn btn-inverse btn-transparent" type="submit">
-                                <strong>GENERATE</strong>
-                            </button>
-                        </center>
-                    </form>
-                </div>
-            </section>
+                          <center>
+                              <button id="generate" class="btn btn-inverse btn-transparent" type="submit">
+                                  <strong>GENERATE</strong>
+                              </button>
+                          </center>
+                      </form>
+                    </div>
+                </section>
+            </div>
         </div>
     </div>
-</div>
           
 
 <script type="text/javascript">
