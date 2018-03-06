2e0cf5a7a9b4cf3f851e77a280f2c3023a22e19b
algreg1998 <algreg17@gmail.com>
2018-03-06 11:27:38 +0800

    tweaked the checkout function

--- application/models/MCheckout.php
+++ application/models/MCheckout.php
@@ -19,12 +19,18 @@
 		public function showCheckout ($id) {
 			$this->db->select("*");
 			$this->db->from("checkout");
-			$this->db->join("cart", "cart.checkoutId = checkout.checkId", "right");
+			$this->db->from("cart");
+			// $this->db->join("cart", "cart.checkoutId = checkout.checkId", "right");
 			$this->db->join("ticket_type as tt","tt.ticket_type_id = cart.ticket_id","left");
 			$this->db->join("event_info as ei","tt.event_id = ei.event_id","left");
 			$this->db->where("checkout.account_id", $id);
+			$this->db->where("cart.checkoutId = checkout.checkId");
+			$this->db->group_by("checkout.checkId");
+
 			
 			$query = $this->db->get();
+			// echo $this->db->last_query();
+			// die();
 			return $query->result();
 		}
 	}
