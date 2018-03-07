65dda778c5ffa920d6e1b2dfd09163966c761cc6
Pancakes13 <neil.llenes@gmail.com>
2018-03-07 20:49:25 +0800

    Seperated and modified content and style! Added Images

--- application/views/vAboutUs.php
+++ application/views/vAboutUs.php
@@ -45,9 +45,13 @@
                 padding: 3em;
                 background-color: rgba(255, 255, 255, 0.5); 
             }
-            #dailyEvents{
+            .dailyEvents{
                 text-align: center;
                 color:white;
+                font-size: 20px;
+            }
+            .row{
+                margin-bottom: 5%;
             }
         </style>
     </head>
@@ -64,24 +68,74 @@
         </nav>
         <div class = "row">
             <div class = "col-sm-3"></div>
-            <div class = "col-sm-6 div-content">
-                <h1>About US!</h1>
-                <br>
-                <hr>
-                <br>
-                <div class = "content">
-                    <div>
-                        <img src="<?php echo base_url(CustomizationManager::$images->LOGO_LIGHT)?>" style="display: block; margin-left: auto; margin-right: auto; height:200px; width:350px;">
-                    </div>
-                    <div id="dailyEvents" >Daily Events is a joint effort project made by students from the University of 
-                    San Carlos in line with their Software Engineering course.
-                    The team composes of 4th year BSIT students from University of San Carlos, together with 
-                    their teacher, a working professional in the field to guide them.
-                    
+                <div class = "col-sm-6 div-content">
+                    <h1>About US!</h1>
+                    <br>
+                    <hr>
+                    <br>
+                    <div class = "content">
+                        
+                        <div>
+                            <img src="<?php echo base_url(CustomizationManager::$images->LOGO_LIGHT)?>" style="display: block; margin-left: auto; margin-right: auto; height:200px; width:350px;">
+                        </div>
+                        <div class="dailyEvents" >Daily Events is a joint effort project made by students from the University of 
+                        San Carlos in line with their Software Engineering course.
+                        The team composes of 4th year BSIT students from University of San Carlos, together with 
+                        their teacher, a working professional in the field to guide them.
+                        
+                        </div>
+                        
                     </div>
                 </div>
+
             </div>
         </div>
+
+        <div class = "row">
+            <div class = "col-sm-2"></div>
+                <div class = "col-sm-8 div-content">
+                    <div class = "content" style="height:450px;">
+                        
+                        <div align="center">
+                                <img class="img-circle" src="<?php echo base_url(CustomizationManager::$images->PARTY_LOGO)?>" style="height:200px; width:200px; display:inline-block; margin-right:40px;">
+                                <img class="img-circle" src="<?php echo base_url(CustomizationManager::$images->TICKET_LOGO)?>" style="height:200px; width:200px; display:inline-block; margin-right:40px;">
+                                <img class="img-circle" src="<?php echo base_url(CustomizationManager::$images->PARTY_LOGO2)?>" style="height:200px; width:200px; display:inline-block;">
+                            </div>
+                            <div class="dailyEvents" style="width:33%; float:left;">
+                                This web application allows people to connect together through events and give them an accessible way for them to look for events.
+                            </div>
+                            <div class="dailyEvents" style="width:33%; float:left;">
+                                This application also allows users a hassle-free way to purchase tickets.
+                            </div>
+                            <div class="dailyEvents" style="width:33%; float:left;">
+                                This application gives clients an easy way to bring events to potential customers.
+                            </div>
+                            
+                        </div>
+                        
+                    </div>
+
+            </div>
+        </div>
+
+        <div class = "row">
+            <div class = "col-sm-2"></div>
+                <div class = "col-sm-8 div-content">
+                    <div class = "content">
+                        
+                    <div align="center" style="margin-bottom: 20px;">
+                        <img src="<?php echo base_url(CustomizationManager::$images->CLASS_LOGO)?>" style="display: block; margin-left: auto; margin-right: auto; height:250px; width:400px;">
+                    </div>
+                    <div class="dailyEvents">
+                        Lastly, I would like to give credit to the people who have made this application possible. No matter how small or big the contribution they had to this project, their laughs, their blessings, their perseverance, their ideas and even their time, understanding that the entire class still had other matters to work on such as their thesis, helped all of us no matter how of a burden or hardship it may be, yet they persevered through and through.
+                    </div>
+                        
+                    </div>
+
+            </div>
+        </div>
+
+        
     </body>
 </html>
 
