cbaf7c8f3cac782e1f2eb45b39bd8b6917dff695
erynvinnice <erynvinnice@gmail.com>
2018-03-08 00:00:41 +0800

    fixed issue #223

--- application/views/vEvents.php
+++ application/views/vEvents.php
@@ -538,8 +538,8 @@
                 </div>
 
                 <div class="form-group">
-                    <label for="contact no">Contact Number (09XXXXXXXXX) </label>
-                    <input type="text" <?php  echo 'value="'.$in->contact_no.'"';?>  pattern="^(09)\d{9}$" class="form-control" name="contact" id="email" required="">
+                    <label for="contact no">Contact Number (09XXXXXXXXX) <i>(if mobile no.: 09XXXXXXXXX; if telephone no.: XXX-XXXX)</i> </label>
+                    <input type="text" <?php  echo 'value="'.$in->contact_no.'"';?>  pattern="^(09)\d{2}-\d{3}-\d{4}$|^\d{3}-\d{4}$" class="form-control" name="contact" id="email" required="">
                 </div>
                 <div class="form-group">
                     <label for="username">Username</label>
