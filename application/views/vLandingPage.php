e0fdd9964b5d4a82c852df6b5975ed5b42918f19
Leonille Christie Lavador <leonillechristie@gmail.com>
2018-03-14 22:41:44 +0800

    removed sort by date and price on landing page

--- application/views/vLandingPage.php
+++ application/views/vLandingPage.php
@@ -152,42 +152,6 @@
       <div class="content-area recent-property" style="padding-bottom: 60px; background-color: rgb(252, 252, 252);">
          <div class="container">
              <div class="row">
-                <div class="col-md-12  padding-top-40 properties-page">
-                    <div class="col-xs-10 page-subheader sorting pl0">
-                        <ul class="sort-by-list">
-                            <li class="active">
-                                <a href="javascript:void(0);" class="order_by_date" data-orderby="property_date" data-order="ASC">
-                                    <?php echo CustomizationManager::$strings->LANDING_PAGE_SORT_BY_DATE ?> <i class="fa fa-sort-amount-asc"></i>
-                                </a>
-                            </li>
-                            <li class="">
-                                <a href="javascript:void(0);" class="order_by_price" data-orderby="property_price" data-order="DESC">
-                                    <?php echo CustomizationManager::$strings->LANDING_PAGE_SORT_BY_PRICE ?> <i class="fa fa-sort-numeric-desc"></i>
-                                </a>
-                            </li>
-                        </ul> <!-- END OF SORT BY LIST-->
-
-                        <!--  <div class="items-per-page">
-                                    <label for="items_per_page"><b><?php echo CustomizationManager::$strings->LANDING_PAGE_EVENTS_PER_PAGE ?></b></label>
-                                    <div class="sel">
-                                        <select id="items_per_page" name="per_page">
-                                            <option value="3">3</option>
-                                            <option value="6">6</option>
-                                            <option value="9">9</option>
-                                            <option selected="selected" value="12">12</option>
-                                            <option value="15">15</option>
-                                            <option value="30">30</option>
-                                            <option value="45">45</option>
-                                            <option value="60">60</option>
-                                        </select>
-                                    </div><!--/ .sel-->
-                        <!--  </div> --><!--/ .items-per-page--> 
-                    </div>
-                   <!--  <div class="col-xs-2 layout-switcher">
-                            <a class="layout-list" href="javascript:void(0);"> <i class="fa fa-th-list"></i>  </a>
-                            <a class="layout-grid active" href="javascript:void(0);"> <i class="fa fa-th"></i> </a>
-                    </div><!--/ .layout-switcher--> 
-                </div>
 
                 <div class="col-md-12 ">
                     <div id="list-type" class="proerty-th">
@@ -226,8 +190,7 @@
                                                 
                                                                 echo '</h3></a>';
 
-                                                                // echo '<div class="item-thumb">
-                                                                // <a href="'.site_url().'/event/CEvent/displayEventDetails/'.$event->event_id.'"><img style="clip: rect(0px,100px,100px,0px); height:100px;" src="'.base_url($event->event_picture).'"></a></div>'; 
+                                                        
                                                                 
                                                                 echo '<div style="height:130px;"><h5>Where: '.$event->event_venue.', '.$event->location_name.', '.$event->region_code.'</h5>';
 
